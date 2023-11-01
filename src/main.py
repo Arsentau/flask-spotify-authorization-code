2ed2a57611c867423cbf80758542267c411d2364
Nabor Torres <arsentau@gmail.com>
2023-11-01 10:13:08 -0300

    First approach

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+from spotify.auth import get_access_token
+from spotify.podcast import get_user_podcasts
+from os import getenv
+
+
 def main():
-    print("Hello World")
+    client_id = getenv('SPOTIFY_CLIENT_ID')
+    try:
+        token = get_access_token(client_id)
+
+    except Exception as e:
+        print(e)
+    try:
+        podcast = get_user_podcasts(client_id, token)
+        print(podcast)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
